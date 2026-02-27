40c14f7581140b1b6f6145cb5e7d19d7c45a2309
nathan@gmail.com <nat@alutq>
2026-02-27 06:57:27 +0000

    Update with errand system

--- config/database.php
+++ config/database.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 return [
     'host' => getenv('DB_HOST') ?: '127.0.0.1',
-    'dbname' => getenv('DB_NAME') ?: 'tumami',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASS') ?: '',
+    'dbname' => getenv('DB_NAME') ?: 'nat_tumami',
+    'username' => getenv('DB_USER') ?: 'nat_mtume',
+    'password' => getenv('DB_PASS') ?: 'Qwasa1234',
     'charset' => 'utf8mb4',
 ];
